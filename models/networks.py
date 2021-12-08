707d68866934900f6a7a4199754e00db7a663142
sr-sourabh <sourabhchaudhary012@gmail.com>
2021-12-08 16:43:10 +0530

    test printing

--- models/networks.py
+++ models/networks.py
@@ -228,7 +228,7 @@ class GlobalGenerator(nn.Module):
         model2 = []
         for i in range(n_blocks):
             # model2 += [ResnetBlock(ngf * mult, padding_type=padding_type, activation=activation, norm_layer=norm_layer)]
-            model2 += [SPADEResnetBlock(ngf * mult, ngf * mult, input_nc)]
+            model2 = SPADEResnetBlock(ngf * mult, ngf * mult, input_nc)
         
         ### upsample
         model3 = []
@@ -238,7 +238,7 @@ class GlobalGenerator(nn.Module):
                        norm_layer(int(ngf * mult / 2)), activation]
         model3 += [nn.ReflectionPad2d(3), nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0), nn.Tanh()]
         self.model1 = nn.Sequential(*model1)
-        self.model2 = nn.Sequential(*model2)
+        self.model2 = model2
         self.model3 = nn.Sequential(*model3)
             
     def forward(self, input):
